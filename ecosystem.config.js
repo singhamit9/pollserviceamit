8bf315839259cc0fc02b4a5ba5ab7bb44a3d5f7f
singhamit9 <69334934+singhamit9@users.noreply.github.com>
2025-04-25 21:48:41 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     exec_mode: 'cluster',
     env: {
       NODE_ENV: 'production',
-      EMQX_HOST: 'your-emqx-endpoint.com',
+      EMQX_HOST: 'MQTT-chat-7c6cdb28f96eeaf6.elb.ap-south-1.amazonaws.com',
       PORT: 8080
     }
   }]
