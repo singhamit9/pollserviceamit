4bce07f0e2a53749f5b463e79ceded4114179598
singhamit9 <69334934+singhamit9@users.noreply.github.com>
2025-04-26 13:29:00 +0530

    Update ecosystem.config.js for existing routes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,6 @@ module.exports = {
     exec_mode: 'cluster',
     env: {
       NODE_ENV: 'production',
-      EMQX_HOST: 'MQTT-chat-7c6cdb28f96eeaf6.elb.ap-south-1.amazonaws.com',
       PORT: 8080
     }
   }]
